0f8547755ebead60ffbaa4256978778741c86cdc
Wojciech <wojtek0122@wp.pl>
2020-10-29 23:10:10 +0100

    Added CapitalManager with 2 function: CreateNewCapitalList & LoadDataFromTextFile

---
+++ CapitalManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace The_Hangman_Game
+{
+    class CapitalManager
+    {
+        private List<Capital> _listCapitals;
+
+        public CapitalManager(string TextFilePath)
+        {
+            CreateNewCapitalList();
+            LoadDataFromTextFile(TextFilePath);
+        }
+
+        private void CreateNewCapitalList()
+        {
+            _listCapitals = new List<Capital>();
+        }
+
+        private void LoadDataFromTextFile(string TextFilePath)
+        {
+
+        }
+    }
+}
