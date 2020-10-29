e2e8f06d487a56c93579acf398dfec1ec5a89550
Wojciech <wojtek0122@wp.pl>
2020-10-29 21:33:34 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace The_Hangman_Game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
