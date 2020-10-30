3edf17cbfc8ce44afdd0ff6b4ce09f885464f636
wojtek0122 <wojtek0122@wp.pl>
2020-10-30 09:40:09 +0100

    Implemented function LoadDataFromTextFile from CapitalManager

--- CapitalManager.cs
+++ CapitalManager.cs
@@ -24,7 +24,16 @@ namespace The_Hangman_Game
 
         private void LoadDataFromTextFile(string TextFilePath)
         {
-
+            var fileStream = new FileStream(@"..\DATA\countries_and_capitals.txt", FileMode.Open, FileAccess.Read);
+            using (var streamReader = new StreamReader(fileStream, Encoding.UTF8))
+            {
+                string line;
+                while ((line = streamReader.ReadLine()) != null)
+                {
+                    var _splitCharIndex = line.IndexOf('|', 0);
+                    _listCapitals.Add(new Capital { Country = line.Substring(0, _splitCharIndex - 1), City = line.Substring(_splitCharIndex + 1, line.Length) } );
+                }
+            }
         }
     }
 }
