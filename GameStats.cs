525a3e7de0d8bdb7940cdf3905f4a286a1c7643e
Wojciech <wojtek0122@wp.pl>
2020-11-01 13:52:04 +0100

    Added GameStats class

---
+++ GameStats.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace The_Hangman_Game
+{
+    class GameStats
+    {
+        private DateTime _startGameTime;
+        private DateTime _endGameTime;
+
+        public GameStats() : this(StartLifePoint: 5)
+        {
+
+        }
+
+        public GameStats(int StartLifePoint)
+        {
+            LifePoint = StartLifePoint;
+            _startGameTime = DateTime.Now;
+        }
+
+        public int LifePoint
+        {
+            get; set;
+        }
+
+        public int GetGameTime()
+        {
+            _endGameTime = DateTime.Now;
+            var time = _endGameTime - _startGameTime;
+            return (int)time.TotalSeconds;
+        }
+    }
+}
